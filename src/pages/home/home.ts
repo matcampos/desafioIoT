ee63f8f3ca46906c78a55e777018683793fe4ee3
matheus campos <matheus.alves1998@hotmail.com>
2017-11-08 12:26:06 -0200

    Logica pronta

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,28 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor(public navCtrl: NavController) {
   }
-  data1 = { "available": false, "timestamp": 1510065830 };
+  data1 = { "available": true, "timestamp": 310065830 };
   data2 = { "available": true, "timestamp": 1518065830 };
-
   showContent: boolean = false;
   status: string;
-  tempo: number;
+  tempo: string;
+
+
+  ngOnInit() {
+    this.ionViewDidLoad();
+  }
+
   ionViewDidLoad() {
-    if (this.data1.available) {
-      status = "Disponível";
+
+    if (this.data1.available == true) {
       this.showContent = true;
+      this.status = "Disponível";
+      this.tempo = this.data1.timestamp.toString();
+      if (this.tempo.length == 10){
+        this.tempo = this.tempo.substring(0,2);
+      }
+      if (this.tempo.length == 9){
+        this.tempo = this.tempo.substring(0,1);
+      }
     }
     else {
-      status = "Ocupado";
+      this.status = "Ocupado";
       this.showContent = true;
     }
+    console.log(status);
+  }
+
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
+    this.showContent = true;
   }
 }
